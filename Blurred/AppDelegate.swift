bae123e3d65d7c5ca69c197bbd2690009c50a950
josephcourtney <5942706+josephcourtney@users.noreply.github.com>
2025-04-26 13:44:39 -0400

    swiftlinted

--- Blurred/AppDelegate.swift
+++ Blurred/AppDelegate.swift
@@ -7,59 +7,58 @@
 //
 
 import Cocoa
-import SwiftUI
 import HotKey
+import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    let statusBarController = StatusBarController()
-    
-    var hotKey: HotKey? {
-        didSet {
-            guard let hotKey = hotKey else { return }
-            
-            hotKey.keyDownHandler = {
-                DimManager.sharedInstance.setting.isEnabled.toggle()
-            }
-        }
+  let statusBarController = StatusBarController()
+
+  var hotKey: HotKey? {
+    didSet {
+      guard let hotKey = hotKey else { return }
+
+      hotKey.keyDownHandler = {
+        DimManager.sharedInstance.setting.isEnabled.toggle()
+      }
     }
-    
-    
-    let eventMonitor = EventMonitor(mask: .leftMouseUp) { _ in
-        // Hanlde this without delay
-        DimManager.sharedInstance.dim(runningApplication: NSWorkspace.shared.frontmostApplication, withDelay: false)
+  }
+
+  let eventMonitor = EventMonitor(mask: .leftMouseUp) { _ in
+    // Hanlde this without delay
+    DimManager.sharedInstance.dim(runningApplication: NSWorkspace.shared.frontmostApplication, withDelay: false)
+  }
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    hideDockIcon()
+    setupAutoStartAtLogin()
+    openPrefWindowIfNeeded()
+    setupHotKey()
+    eventMonitor.start()
+  }
+
+  func applicationDidChangeScreenParameters(_ notification: Notification) {
+    DimManager.sharedInstance.dim(runningApplication: NSWorkspace.shared.frontmostApplication)
+  }
+
+  func setupHotKey() {
+    guard let globalKey = UserDefaults.globalKey else { return }
+    hotKey = HotKey(keyCombo: KeyCombo(carbonKeyCode: globalKey.keyCode, carbonModifiers: globalKey.carbonFlags))
+  }
+
+  func openPrefWindowIfNeeded() {
+    if UserDefaults.isOpenPrefWhenOpenApp {
+      PreferencesWindowController.shared.window?.makeKeyAndOrderFront(nil)
+      NSApp.activate(ignoringOtherApps: true)
     }
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        hideDockIcon()
-        setupAutoStartAtLogin()
-        openPrefWindowIfNeeded()
-        setupHotKey()
-        eventMonitor.start()
-    }
-    
-    func applicationDidChangeScreenParameters(_ notification: Notification) {
-        DimManager.sharedInstance.dim(runningApplication: NSWorkspace.shared.frontmostApplication)
-    }
-    
-    func setupHotKey() {
-        guard let globalKey = UserDefaults.globalKey else {return}
-        hotKey = HotKey(keyCombo: KeyCombo(carbonKeyCode: globalKey.keyCode, carbonModifiers: globalKey.carbonFlags))
-    }
-    
-    func openPrefWindowIfNeeded() {
-        if UserDefaults.isOpenPrefWhenOpenApp {
-            PreferencesWindowController.shared.window?.makeKeyAndOrderFront(nil)
-            NSApp.activate(ignoringOtherApps: true)
-        }
-    }
-    
-    func setupAutoStartAtLogin() {
-        let isAutoStart = UserDefaults.isStartWhenLogin
-        Util.setUpAutoStart(isAutoStart: isAutoStart)
-    }
-    
-    func hideDockIcon() {
-        NSApp.setActivationPolicy(.accessory)
-    }
+  }
+
+  func setupAutoStartAtLogin() {
+    let isAutoStart = UserDefaults.isStartWhenLogin
+    Util.setUpAutoStart(isAutoStart: isAutoStart)
+  }
+
+  func hideDockIcon() {
+    NSApp.setActivationPolicy(.accessory)
+  }
 }
--- Blurred/ViewControllers/StatusBarController.swift
+++ Blurred/ViewControllers/StatusBarController.swift
@@ -11,59 +11,59 @@ import Cocoa
 import Combine
 import SwiftUI
 
-class StatusBarController{
-    
-    private let menuStatusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+class StatusBarController {
+
+    private let menuStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     private let slider = NSSlider()
     private var cancellableSet: Set<AnyCancellable> = []
-    
+
     init() {
         setupView()
         setupSlider()
     }
-    
+
     private func setupView() {
         if let button = menuStatusItem.button {
             button.image = #imageLiteral(resourceName: "ico_menu")
-            
+
             let swipeView = StatusBarSwipeToSetAlphaView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: button.frame.size))
             button.addSubview(swipeView)
         }
         menuStatusItem.menu = getContextMenu()
     }
-    
+
     private func setupSlider() {
         slider.minValue = 10.0
         slider.maxValue = 100.0
         slider.doubleValue = DimManager.sharedInstance.setting.alpha
-        
+
         DimManager.sharedInstance.setting.$alpha
             .removeDuplicates()
             .receive(on: DispatchQueue.main)
             .assign(to: \.doubleValue, on: self.slider)
             .store(in: &cancellableSet)
-        
+
         DimManager.sharedInstance.setting.$isEnabled
             .receive(on: DispatchQueue.main)
             .assign(to: \.isEnabled, on: self.slider)
             .store(in: &cancellableSet)
-        
+
         slider.target = self
         slider.action = #selector(sliderChanged)
     }
-    
+
     @objc private func sliderChanged() {
         DimManager.sharedInstance.setting.alpha = slider.doubleValue
     }
-    
+
     private func getContextMenu() -> NSMenu {
         let menu = NSMenu()
         let sliderMenuItem = NSMenuItem()
         let titleEnable = DimManager.sharedInstance.setting.isEnabled ? "Disable".localized : "Enable".localized
-        
+
         let enableButton = NSMenuItem(title: titleEnable, action: #selector(toggleEnable), keyEquivalent: "E")
         enableButton.target = self
-        
+
         DimManager.sharedInstance.setting.$isEnabled
             .receive(on: DispatchQueue.main)
             .sink {[weak enableButton] isEnabled in
@@ -71,7 +71,7 @@ class StatusBarController{
                 enableButton?.title = title
         }
         .store(in: &cancellableSet)
-        
+
         let view = NSView()
         view.setFrameSize(NSSize(width: 100, height: 22))
         view.autoresizingMask = .width
@@ -81,9 +81,9 @@ class StatusBarController{
             NSLayoutConstraint(item: slider, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 16),
             NSLayoutConstraint(item: slider, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -16),
             NSLayoutConstraint(item: slider, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: slider, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: slider, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
         ])
-        
+
         sliderMenuItem.view = view
         menu.addItem(withTitle: "Slide to set Dim level".localized, action: nil, keyEquivalent: "")
         menu.addItem(sliderMenuItem)
@@ -96,11 +96,11 @@ class StatusBarController{
         menu.item(withTitle: "Preferences...".localized)?.target = self
         return menu
     }
-    
+
     @objc private func toggleEnable() {
         DimManager.sharedInstance.setting.isEnabled.toggle()
     }
-    
+
     @objc func openPreferences() {
         PreferencesWindowController.shared.window?.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
